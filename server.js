b123b293f48af63e5144d94c251640281dc058b3
Ayoub E <a.ech-charrat@1-one.fr>
2025-02-13 11:58:52 +0100

    correction tree

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const userRoutes = require("./src/routes/userRoutes");
+const messageRoutes = require("./src/routes/messageRoutes");
 
 const app = express();
 
 app.use(express.json());
 
 app.use("/api/auth", userRoutes);
-app.use("/api/posts", postRoutes);
+app.use("/api/messages", messageRoutes);
+
+app.listen(3300, () => console.log(`Server running on port 3300`));
 
 module.exports = app;
