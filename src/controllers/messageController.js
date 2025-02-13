7f9d6b347a34eeae7ada9cce5d41d73f9fdece03
Yukigana <valentin.debonnefoy@gmail.com>
2025-02-13 10:34:45 +0100

    add: message controller

---
+++ src/controllers/messageController.js
@@ -0,0 +1,46 @@
+const Message = require("../models/messageModel");
+
+exports.getConversationMessages = async (req, res) => {
+  try {
+    const { idRoom } = req.body;
+    if (!idRoom) {
+      return res.status(400).json({ msg: "Id room is required" });
+    }
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
+};
+
+exports.sendMessage = async (req, res) => {
+  try {
+    const { sender, content, idRoom } = req.body;
+    if (!sender || !content || !idRoom) {
+      return res.status(400).json({ msg: "All fields are required" });
+    }
+
+    let message = new Message({
+      sender,
+      content,
+      idRoom,
+      likedBy: [],
+    });
+    message.save();
+    res.send("Message sent successfully");
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
+};
+
+exports.deleteMessage = async (req, res) => {
+  try {
+    const { id } = req.body;
+    if (!id) {
+      return res.status(400).json({ msg: "Id message is required" });
+    }
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
+};
