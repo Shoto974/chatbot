206c17e8a936a70603347795cfab952e62e23afb
jessy fanguimache <jessy.fangui@gmail.com>
2025-02-13 16:58:56 +0100

    Mise en place ws pour les messages

--- server.js
+++ server.js
@@ -4,8 +4,16 @@ const messageRoutes = require("./src/routes/messageRoutes");
 const { PORT } = require("./src/config/env");
 const connectionDB = require("./src/config/db");
 const expressWs = require("express-ws");
+const { addClient } = require("./src/config/wsManager");
 const app = express();
+const cors = require("cors");
 
+app.use(cors());
+//Init serveur ws
+expressWs(app);
+app.ws("/ws", (ws, req) => {
+  addClient(ws);
+});
 app.use(express.json());
 
 connectionDB();
