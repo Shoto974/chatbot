27b04e61752c89df20578b1b1bad17393d6c4469
Ayoub E <a.ech-charrat@1-one.fr>
2025-02-13 12:39:20 +0100

    route isworking$

--- src/controllers/messageController.js
+++ src/controllers/messageController.js
@@ -11,20 +11,20 @@ exports.isWorking = async (req, res) => {
 
 exports.getConversationMessages = async (req, res) => {
   try {
-    const { idRoom } = req.params.idRoom;
+    const idRoom = req.params.idRoom;
     if (!idRoom) {
       return res.status(400).json({ msg: "Id room is required" });
     }
   } catch (err) {
     console.error(err.message);
-    res.status(500).send(error);
+    res.status(500).send(err);
   }
 };
 
 exports.sendMessage = async (req, res) => {
   try {
     const { sender, content } = req.body;
-    const { idRoom } = req.params.idRoom;
+    const idRoom = req.params.idRoom;
 
     if (!sender || !content || !idRoom) {
       return res.status(400).json({ msg: "All fields are required" });
