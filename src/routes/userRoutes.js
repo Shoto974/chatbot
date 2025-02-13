3515461accaddcbdced4b7322eafa0eb37ac7d53
Ayoub E <a.ech-charrat@1-one.fr>
2025-02-13 11:50:39 +0100

    add user routes

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,6 +1,8 @@
 const express = require("express");
-
+const { registerUser, loginUser } = require("../controllers/userController");
 const router = express.Router();
 
-router.get("/login" /* loginController */);
-router.get("/register" /* registerController */);
+router.get("/login", loginUser);
+router.get("/register", registerUser);
+
+module.exports = router;
